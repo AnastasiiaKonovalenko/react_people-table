9c3560f29e7731cbec17b63169d8281f0ab927e4
natya2203 <nastya_2203@ukr.net>
2020-01-05 00:36:40 +0200

    Updates

---
+++ precache-manifest.dca679e546318e6d0ca58cc22f9ace23.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a838173b189817bc4ead550d572e50f8",
+    "url": "/react_people-table/index.html"
+  },
+  {
+    "revision": "c54f6204e95d8c7a7700",
+    "url": "/react_people-table/static/css/main.8b1006da.chunk.css"
+  },
+  {
+    "revision": "d8850e8da6827d39bf6e",
+    "url": "/react_people-table/static/js/2.38a4f940.chunk.js"
+  },
+  {
+    "revision": "c54f6204e95d8c7a7700",
+    "url": "/react_people-table/static/js/main.112b8ef7.chunk.js"
+  },
+  {
+    "revision": "c6b2e506936d419d6d52",
+    "url": "/react_people-table/static/js/runtime~main.b2b73302.js"
+  }
+]);
